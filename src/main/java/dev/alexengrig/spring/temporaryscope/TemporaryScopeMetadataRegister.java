e0f4779390a4aa34bd0bccc7dc8f6824a2da8c1c
Grig Alex <alexengrigdev@gmail.com>
2021-07-06 00:27:06 +0300

    Add attributes to xml bean definition

--- src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataRegister.java
+++ src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataRegister.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import java.lang.annotation.Annotation;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
+import java.util.function.Function;
 
 public class TemporaryScopeMetadataRegister implements BeanFactoryPostProcessor {
 
@@ -26,11 +28,15 @@ public class TemporaryScopeMetadataRegister implements BeanFactoryPostProcessor 
                 if (beanDefinition instanceof AnnotatedBeanDefinition annotatedBeanDefinition) {
                     AnnotatedTypeMetadata metadata = getMetadata(annotatedBeanDefinition);
                     if (metadata.isAnnotated(TemporaryScope.CLASS_NAME)) {
-                        TemporaryScopeMetadata temporaryScopeMetadata = getTemporaryScopeMetadata(metadata);
-                        temporaryScopeMetadataHolder.put(beanName, temporaryScopeMetadata);
+                        temporaryScopeMetadataHolder.put(beanName, getTemporaryScopeMetadata(metadata));
                     }
+                } else if (beanDefinition instanceof GenericBeanDefinition genericBeanDefinition) {
+                    TemporaryScopeGetter getter = TemporaryScopeGetter.fromXmlMetaAttributes(beanDefinition::getAttribute);
+                    long amount = getter.value();
+                    ChronoUnit unit = getter.unit();
+                    temporaryScopeMetadataHolder.put(beanName, new TemporaryScopeMetadata(amount, unit));
                 } else {
-                    //TODO: Add overriding default value + setting via xml
+                    //TODO: Add overriding default value
                     temporaryScopeMetadataHolder.put(beanName, new TemporaryScopeMetadata(1000, ChronoUnit.MILLIS));
                 }
             }
@@ -47,25 +53,25 @@ public class TemporaryScopeMetadataRegister implements BeanFactoryPostProcessor 
 
     private TemporaryScopeMetadata getTemporaryScopeMetadata(AnnotatedTypeMetadata metadata) {
         Map<String, Object> attributes = metadata.getAnnotationAttributes(TemporaryScope.CLASS_NAME);
-        TemporaryScopeGetter getter = new TemporaryScopeGetter(attributes);
+        TemporaryScopeGetter getter = TemporaryScopeGetter.fromAnnotationAttributes(attributes);
         long amount = getter.value();
         ChronoUnit unit = getter.unit();
         return new TemporaryScopeMetadata(amount, unit);
     }
 
     @SuppressWarnings("ClassExplicitlyAnnotation")
-    private static record TemporaryScopeGetter(Map<String, Object> attributes) implements TemporaryScope {
+    private static record TemporaryScopeGetter(long value, ChronoUnit unit) implements TemporaryScope {
 
-        @Override
-        public long value() {
-            Object value = attributes.get("value");
-            return (long) value;
+        public static TemporaryScopeGetter fromAnnotationAttributes(Map<String, Object> attributes) {
+            long value = (long) attributes.get("value");
+            ChronoUnit unit = (ChronoUnit) attributes.get("unit");
+            return new TemporaryScopeGetter(value, unit);
         }
 
-        @Override
-        public ChronoUnit unit() {
-            Object unit = attributes.get("unit");
-            return (ChronoUnit) unit;
+        public static TemporaryScopeGetter fromXmlMetaAttributes(Function<String, Object> getter) {
+            long value = Long.parseLong((String) getter.apply(TemporaryScope.CLASS_NAME + ".value"));
+            ChronoUnit unit = ChronoUnit.valueOf((String) getter.apply(TemporaryScope.CLASS_NAME + ".unit"));
+            return new TemporaryScopeGetter(value, unit);
         }
 
         @Override
