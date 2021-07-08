0cb74a03498a923c2984bdb8a6141cbf889565ec
Grig Alex <alexengrigdev@gmail.com>
2021-07-08 19:52:09 +0300

    Create SingletonTemporaryScopeMetadataHolder with TemporaryScopeMetadataHolder interface

---
+++ src/main/java/dev/alexengrig/spring/temporaryscope/SingletonTemporaryScopeMetadataHolder.java
@@ -0,0 +1,39 @@
+package dev.alexengrig.spring.temporaryscope;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class SingletonTemporaryScopeMetadataHolder implements TemporaryScopeMetadataHolder {
+
+    private static final Object INSTANCE_LOCK = new Object();
+    private static volatile SingletonTemporaryScopeMetadataHolder INSTANCE;
+
+    private final Map<String, TemporaryScopeMetadata> metadataByName = new ConcurrentHashMap<>();
+
+    private SingletonTemporaryScopeMetadataHolder() {
+    }
+
+    public static SingletonTemporaryScopeMetadataHolder instance() {
+        SingletonTemporaryScopeMetadataHolder instance = INSTANCE;
+        if (instance == null) {
+            synchronized (INSTANCE_LOCK) {
+                instance = INSTANCE;
+                if (instance == null) {
+                    INSTANCE = instance = new SingletonTemporaryScopeMetadataHolder();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void put(String beanName, TemporaryScopeMetadata scopeMetadata) {
+        metadataByName.put(beanName, scopeMetadata);
+    }
+
+    @Override
+    public TemporaryScopeMetadata get(String beanName) {
+        return metadataByName.get(beanName);
+    }
+
+}
--- src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataHolder.java
+++ src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataHolder.java
@@ -1,37 +1,9 @@
 package dev.alexengrig.spring.temporaryscope;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+public interface TemporaryScopeMetadataHolder {
 
-public final class TemporaryScopeMetadataHolder {
+    void put(String beanName, TemporaryScopeMetadata scopeMetadata);
 
-    private static final Object INSTANCE_LOCK = new Object();
-    private static volatile TemporaryScopeMetadataHolder INSTANCE;
-
-    private final Map<String, TemporaryScopeMetadata> metadataByName = new ConcurrentHashMap<>();
-
-    private TemporaryScopeMetadataHolder() {
-    }
-
-    public static TemporaryScopeMetadataHolder instance() {
-        TemporaryScopeMetadataHolder instance = INSTANCE;
-        if (instance == null) {
-            synchronized (INSTANCE_LOCK) {
-                instance = INSTANCE;
-                if (instance == null) {
-                    INSTANCE = instance = new TemporaryScopeMetadataHolder();
-                }
-            }
-        }
-        return instance;
-    }
-
-    public TemporaryScopeMetadata get(String name) {
-        return metadataByName.get(name);
-    }
-
-    public void put(String name, TemporaryScopeMetadata metadata) {
-        metadataByName.put(name, metadata);
-    }
+    TemporaryScopeMetadata get(String beanName);
 
 }
