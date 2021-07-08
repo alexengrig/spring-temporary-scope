ddfc67fa7ce2eb09cbc47c9bbbb342a9042c13b3
Grig Alex <alexengrigdev@gmail.com>
2021-07-08 19:22:52 +0300

    Refactor

--- src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataRegister.java
+++ src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadataRegister.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import java.lang.annotation.Annotation;
@@ -15,35 +14,46 @@ import java.util.function.Function;
 
 public class TemporaryScopeMetadataRegister implements BeanFactoryPostProcessor {
 
-    private static final String UNSUPPORTED_SCOPE_MESSAGE = "Scope \"%s\" supports only Annotation-based configuration"
-            .formatted(TemporaryScope.SCOPE_NAME);
-
     private final TemporaryScopeMetadataHolder temporaryScopeMetadataHolder = TemporaryScopeMetadataHolder.instance();
+
+    private void registerMetadata(String beanName, TemporaryScopeMetadata metadata) {
+        temporaryScopeMetadataHolder.put(beanName, metadata);
+    }
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         for (String beanName : beanFactory.getBeanDefinitionNames()) {
             BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
-            if (TemporaryScope.SCOPE_NAME.equals(beanDefinition.getScope())) {
-                if (beanDefinition instanceof AnnotatedBeanDefinition annotatedBeanDefinition) {
-                    AnnotatedTypeMetadata metadata = getMetadata(annotatedBeanDefinition);
-                    if (metadata.isAnnotated(TemporaryScope.CLASS_NAME)) {
-                        temporaryScopeMetadataHolder.put(beanName, getTemporaryScopeMetadata(metadata));
-                    }
-                } else if (beanDefinition instanceof GenericBeanDefinition genericBeanDefinition) {
-                    TemporaryScopeGetter getter = TemporaryScopeGetter.fromXmlMetaAttributes(beanDefinition::getAttribute);
-                    long amount = getter.value();
-                    ChronoUnit unit = getter.unit();
-                    temporaryScopeMetadataHolder.put(beanName, new TemporaryScopeMetadata(amount, unit));
-                } else {
-                    //TODO: Add overriding default value
-                    temporaryScopeMetadataHolder.put(beanName, new TemporaryScopeMetadata(1000, ChronoUnit.MILLIS));
-                }
+            if (isTargetScope(beanDefinition)) {
+                process(beanName, beanDefinition);
             }
         }
     }
 
-    private AnnotatedTypeMetadata getMetadata(AnnotatedBeanDefinition annotatedBeanDefinition) {
+    private boolean isTargetScope(BeanDefinition beanDefinition) {
+        return TemporaryScope.SCOPE_NAME.equals(beanDefinition.getScope());
+    }
+
+    private void process(String beanName, BeanDefinition beanDefinition) {
+        if (beanDefinition instanceof AnnotatedBeanDefinition annotatedBeanDefinition) {
+            processAnnotatedBean(beanName, annotatedBeanDefinition);
+        } else {
+            //TODO check other configuration types
+            processXmlBean(beanName, beanDefinition);
+        }
+    }
+
+    private void processAnnotatedBean(String beanName, AnnotatedBeanDefinition annotatedBeanDefinition) {
+        AnnotatedTypeMetadata beanMetadata = getBeanMetadata(annotatedBeanDefinition);
+        if (beanMetadata.isAnnotated(TemporaryScope.CLASS_NAME)) {
+            TemporaryScopeMetadata scopeMetadata = getScopeMetadata(beanMetadata);
+            registerMetadata(beanName, scopeMetadata);
+        } else {
+            //TODO @Scope("temporary")
+        }
+    }
+
+    private AnnotatedTypeMetadata getBeanMetadata(AnnotatedBeanDefinition annotatedBeanDefinition) {
         AnnotatedTypeMetadata metadata = annotatedBeanDefinition.getFactoryMethodMetadata();
         if (metadata != null) {
             return metadata;
@@ -51,27 +61,43 @@ public class TemporaryScopeMetadataRegister implements BeanFactoryPostProcessor 
         return annotatedBeanDefinition.getMetadata();
     }
 
-    private TemporaryScopeMetadata getTemporaryScopeMetadata(AnnotatedTypeMetadata metadata) {
-        Map<String, Object> attributes = metadata.getAnnotationAttributes(TemporaryScope.CLASS_NAME);
-        TemporaryScopeGetter getter = TemporaryScopeGetter.fromAnnotationAttributes(attributes);
+    private TemporaryScopeMetadata getScopeMetadata(AnnotatedTypeMetadata beanMetadata) {
+        TemporaryScopeValueProvider getter = TemporaryScopeValueProvider.from(beanMetadata);
+        long amount = getter.value();
+        ChronoUnit unit = getter.unit();
+        return new TemporaryScopeMetadata(amount, unit);
+    }
+
+    private void processXmlBean(String beanName, BeanDefinition beanDefinition) {
+        TemporaryScopeMetadata scopeMetadata = getScopeMetadata(beanDefinition);
+        registerMetadata(beanName, scopeMetadata);
+    }
+
+    private TemporaryScopeMetadata getScopeMetadata(BeanDefinition beanDefinition) {
+        TemporaryScopeValueProvider getter = TemporaryScopeValueProvider.from(beanDefinition::getAttribute);
         long amount = getter.value();
         ChronoUnit unit = getter.unit();
         return new TemporaryScopeMetadata(amount, unit);
     }
 
     @SuppressWarnings("ClassExplicitlyAnnotation")
-    private static record TemporaryScopeGetter(long value, ChronoUnit unit) implements TemporaryScope {
+    private static record TemporaryScopeValueProvider(long value, ChronoUnit unit) implements TemporaryScope {
 
-        public static TemporaryScopeGetter fromAnnotationAttributes(Map<String, Object> attributes) {
-            long value = (long) attributes.get("value");
-            ChronoUnit unit = (ChronoUnit) attributes.get("unit");
-            return new TemporaryScopeGetter(value, unit);
+        public static TemporaryScopeValueProvider from(AnnotatedTypeMetadata beanMetadata) {
+            Map<String, Object> attributes = beanMetadata.getAnnotationAttributes(TemporaryScope.CLASS_NAME);
+            if (attributes == null) {
+                throw new IllegalArgumentException("Bean doesn't have @TemporaryScope annotation.");
+            }
+            long value = (long) attributes.get(TemporaryScope.VALUE_NAME);
+            ChronoUnit unit = (ChronoUnit) attributes.get(TemporaryScope.UNIT_NAME);
+            return new TemporaryScopeValueProvider(value, unit);
         }
 
-        public static TemporaryScopeGetter fromXmlMetaAttributes(Function<String, Object> getter) {
+        public static TemporaryScopeValueProvider from(Function<String, Object> getter) {
+            //TODO NPE
             long value = Long.parseLong((String) getter.apply(TemporaryScope.VALUE_PROPERTY));
             ChronoUnit unit = ChronoUnit.valueOf((String) getter.apply(TemporaryScope.UNIT_PROPERTY));
-            return new TemporaryScopeGetter(value, unit);
+            return new TemporaryScopeValueProvider(value, unit);
         }
 
         @Override
