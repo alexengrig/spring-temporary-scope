d5b34c31c127df63202c7d094590ea7efd571ee2
Grig Alex <alexengrigdev@gmail.com>
2021-07-08 20:30:17 +0300

    Update TemporaryScopeMetadata

--- src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadata.java
+++ src/main/java/dev/alexengrig/spring/temporaryscope/TemporaryScopeMetadata.java
@@ -1,6 +1,16 @@
 package dev.alexengrig.spring.temporaryscope;
 
+import java.time.LocalDateTime;
 import java.time.temporal.TemporalUnit;
 
 public record TemporaryScopeMetadata(long amount, TemporalUnit unit) {
+
+    public LocalDateTime createExpiredAt() {
+        return createExpiredAt(LocalDateTime.now());
+    }
+
+    public LocalDateTime createExpiredAt(LocalDateTime dateTime) {
+        return dateTime.plus(amount, unit);
+    }
+
 }
