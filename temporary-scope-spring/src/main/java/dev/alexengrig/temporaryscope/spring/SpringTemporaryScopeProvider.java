2fdf28dc8abd3d8af0ea68eee9534c94e4f23513
Grig Alex <alexengrigdev@gmail.com>
2021-07-10 23:42:09 +0300

    Create SpringTemporaryScopeProvider

---
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeProvider.java
@@ -0,0 +1,46 @@
+package dev.alexengrig.temporaryscope.spring;
+
+import dev.alexengrig.temporaryscope.SingletonTemporaryScopeProvider;
+import dev.alexengrig.temporaryscope.TemporaryScopeProvider;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SpringTemporaryScopeProvider implements Scope {
+
+    private final TemporaryScopeProvider scopeProvider = SingletonTemporaryScopeProvider.instance();
+    private final Map<String, Runnable> callbackByName = new ConcurrentHashMap<>();
+
+    @Override
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        return scopeProvider.get(name, objectFactory::getObject);
+    }
+
+    @Override
+    public Object remove(String name) {
+        Object object = scopeProvider.remove(name);
+        Runnable callback = callbackByName.get(name);
+        if (callback != null) {
+            callback.run();
+        }
+        return object;
+    }
+
+    @Override
+    public void registerDestructionCallback(String name, Runnable callback) {
+        callbackByName.put(name, callback);
+    }
+
+    @Override
+    public Object resolveContextualObject(String key) {
+        return null;
+    }
+
+    @Override
+    public String getConversationId() {
+        return null;
+    }
+
+}
