1ec0cfa2eb6c94772c4e317d42ccb85e001c1d9d
Grig Alex <alexengrigdev@gmail.com>
2021-07-12 00:21:20 +0300

    Add beans to SpringTemporaryScopeConfiguration

--- temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeConfiguration.java
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeConfiguration.java
@@ -1,5 +1,7 @@
 package dev.alexengrig.temporaryscope.spring;
 
+import org.springframework.context.annotation.Bean;
+
 public class SpringTemporaryScopeConfiguration {
 
     public static final String SCOPE_NAME = "temporary";
@@ -7,5 +9,15 @@ public class SpringTemporaryScopeConfiguration {
     public static final String VALUE_PROPERTY = Temporary.CLASS_NAME + ".value";
 
     public static final String UNIT_PROPERTY = Temporary.CLASS_NAME + ".unit";
+
+    @Bean
+    static SpringTemporaryScopeRegistrar temporaryScopeRegister() {
+        return new SpringTemporaryScopeRegistrar();
+    }
+
+    @Bean
+    static SpringTemporaryScopeMetadataRegistrar temporaryScopeMetadataRegister() {
+        return new SpringTemporaryScopeMetadataRegistrar();
+    }
 
 }
