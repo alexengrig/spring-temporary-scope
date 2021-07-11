2239ba5fb0cf084cbae050324599d2e34228c2e2
Grig Alex <alexengrigdev@gmail.com>
2021-07-11 19:14:08 +0300

    Create Temporary annotation

---
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/Temporary.java
@@ -0,0 +1,21 @@
+package dev.alexengrig.temporaryscope.spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.time.temporal.ChronoUnit;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Temporary {
+
+    String CLASS_NAME = Temporary.class.getName();
+    String VALUE_NAME = "value";
+    String UNIT_NAME = "unit";
+
+    long value();
+
+    ChronoUnit unit() default ChronoUnit.MILLIS;
+
+}
