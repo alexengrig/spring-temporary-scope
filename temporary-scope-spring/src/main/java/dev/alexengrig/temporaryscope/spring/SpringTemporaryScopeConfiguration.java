1ee66960f413866946c155de24a574a901d7d20b
Grig Alex <alexengrigdev@gmail.com>
2021-07-11 23:42:42 +0300

    Create SpringTemporaryScopeConfiguration

---
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeConfiguration.java
@@ -0,0 +1,11 @@
+package dev.alexengrig.temporaryscope.spring;
+
+public class SpringTemporaryScopeConfiguration {
+
+    public static final String SCOPE_NAME = "temporary";
+
+    public static final String VALUE_PROPERTY = Temporary.CLASS_NAME + ".value";
+
+    public static final String UNIT_PROPERTY = Temporary.CLASS_NAME + ".unit";
+
+}
--- temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/TemporaryScopeConfiguration.java
+++
@@ -1,7 +0,0 @@
-package dev.alexengrig.temporaryscope.spring;
-
-public class TemporaryScopeConfiguration {
-
-    public static final String SCOPE_NAME = "temporary";
-
-}
