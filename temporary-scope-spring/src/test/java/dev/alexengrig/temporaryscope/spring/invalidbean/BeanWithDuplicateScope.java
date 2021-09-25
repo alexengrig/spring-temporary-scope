f32b778aa08c72b3087f1ba7933bbaa945c1d607
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 22:55:42 +0300

    Create test invalid beans

---
+++ temporary-scope-spring/src/test/java/dev/alexengrig/temporaryscope/spring/invalidbean/BeanWithDuplicateScope.java
@@ -0,0 +1,6 @@
+package dev.alexengrig.temporaryscope.spring.invalidbean;
+
+import dev.alexengrig.temporaryscope.spring.WithCreatedAt;
+
+public class BeanWithDuplicateScope extends WithCreatedAt {
+}
