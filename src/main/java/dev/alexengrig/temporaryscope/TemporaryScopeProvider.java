60e055e790ec78288a8b510af6dfa14809a70d9b
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 15:52:18 +0300

    Add cast-methods to TemporaryScopeProvider

--- src/main/java/dev/alexengrig/temporaryscope/TemporaryScopeProvider.java
+++ src/main/java/dev/alexengrig/temporaryscope/TemporaryScopeProvider.java
@@ -6,6 +6,16 @@ public interface TemporaryScopeProvider {
 
     Object get(String beanName, Supplier<Object> beanProducer);
 
+    @SuppressWarnings("unchecked")
+    default <T> T getAs(String beanName, Supplier<? extends T> beanProducer) throws ClassCastException {
+        return (T) get(beanName, beanProducer::get);
+    }
+
     Object remove(String beanName);
+
+    @SuppressWarnings("unchecked")
+    default <T> T removeAs(String beanName) throws ClassCastException {
+        return (T) remove(beanName);
+    }
 
 }
