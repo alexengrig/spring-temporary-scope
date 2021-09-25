188780c56470d0d7cbc04066d7f339a3137bdd0b
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 21:19:12 +0300

    Fix SpringTemporaryScopeTest

--- temporary-scope-spring/src/test/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeTest.java
+++ temporary-scope-spring/src/test/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeTest.java
@@ -1,17 +1,33 @@
 package dev.alexengrig.temporaryscope.spring;
 
-import dev.alexengrig.temporaryscope.spring.bean.*;
+import dev.alexengrig.temporaryscope.spring.bean.TemporaryBean;
+import dev.alexengrig.temporaryscope.spring.bean.TemporaryComponent;
+import dev.alexengrig.temporaryscope.spring.bean.TemporaryScopeBean;
+import dev.alexengrig.temporaryscope.spring.bean.TemporaryScopeComponent;
+import dev.alexengrig.temporaryscope.spring.bean.WithCreatedAt;
+import dev.alexengrig.temporaryscope.spring.bean.XmlBean;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.fail;
 
-@SpringJUnitConfig(SpringTemporaryScopeTest.Config.class)
+@SpringJUnitConfig(
+        value = SpringTemporaryScopeTest.Config.class,
+        initializers = SpringTemporaryScopeInitializer.class
+)
 public class SpringTemporaryScopeTest {
 
     public static final long AMOUNT = 1000;
