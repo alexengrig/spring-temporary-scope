15e4d3c8327190fac97d5068c27f0495c0e37351
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 17:29:13 +0300

    Update SpringTemporaryScopeProvider

--- temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeProvider.java
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeProvider.java
@@ -1,6 +1,5 @@
 package dev.alexengrig.temporaryscope.spring;
 
-import dev.alexengrig.temporaryscope.SingletonTemporaryScopeProvider;
 import dev.alexengrig.temporaryscope.TemporaryScopeProvider;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
@@ -10,17 +9,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class SpringTemporaryScopeProvider implements Scope {
 
-    private final TemporaryScopeProvider scopeProvider = SingletonTemporaryScopeProvider.instance();
-    private final Map<String, Runnable> callbackByName = new ConcurrentHashMap<>();
+    private final TemporaryScopeProvider provider;
+    private final Map<String, Runnable> callbackByName;
+
+    public SpringTemporaryScopeProvider(TemporaryScopeProvider scopeProvider) {
+        this(scopeProvider, new ConcurrentHashMap<>());
+    }
+
+    public SpringTemporaryScopeProvider(TemporaryScopeProvider scopeProvider, Map<String, Runnable> callbackMap) {
+        this.provider = scopeProvider;
+        this.callbackByName = callbackMap;
+    }
 
     @Override
     public Object get(String name, ObjectFactory<?> objectFactory) {
-        return scopeProvider.get(name, objectFactory::getObject);
+        return provider.get(name, objectFactory::getObject);
     }
 
     @Override
     public Object remove(String name) {
-        Object object = scopeProvider.remove(name);
+        Object object = provider.remove(name);
         Runnable callback = callbackByName.get(name);
         if (callback != null) {
             callback.run();
