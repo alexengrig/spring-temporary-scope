f1115cb833f7ca086833d5b5e9c865b579fc80d7
Grig Alex <alexengrigdev@gmail.com>
2021-09-25 19:58:25 +0300

    Update SpringTemporaryScopeRegistrar

--- temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeRegistrar.java
+++ temporary-scope-spring/src/main/java/dev/alexengrig/temporaryscope/spring/SpringTemporaryScopeRegistrar.java
@@ -4,7 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
+import java.util.logging.Logger;
+
+import static dev.alexengrig.temporaryscope.spring.SpringTemporaryScopeConfiguration.SCOPE_NAME;
+
 public class SpringTemporaryScopeRegistrar implements BeanFactoryPostProcessor {
+
+    private static final Logger LOGGER = Logger.getLogger(SpringTemporaryScopeRegistrar.class.getName());
 
     private final SpringTemporaryScopeProvider scopeProvider;
 
@@ -14,7 +20,8 @@ public class SpringTemporaryScopeRegistrar implements BeanFactoryPostProcessor {
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        beanFactory.registerScope(SpringTemporaryScopeConfiguration.SCOPE_NAME, scopeProvider);
+        LOGGER.info("Register scope '" + SCOPE_NAME + "'");
+        beanFactory.registerScope(SCOPE_NAME, scopeProvider);
     }
 
 }
